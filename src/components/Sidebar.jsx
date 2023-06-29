a65fa165cd2a3dbdad2d5a559ca00db3b4f2ad7c
Guilherme De Souza Silva <guilherme.silva@authoritypartners.com>
2023-06-29 14:42:28 -0300

    refactor: add avatar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "phosphor-react";
 
 import styles from './Sidebar.module.css';
+import { Avatar } from "./Avatar";
 
 
 export function Sidebar() {
@@ -11,11 +12,7 @@ export function Sidebar() {
         src='https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50' 
       />
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/iuG122.png"
-          alt=""
-        />
+        <Avatar src="https://github.com/iuG122.png" />
         <strong>Guilherme Silva</strong>
         <span>Full Stack Developer</span>
       </div>
